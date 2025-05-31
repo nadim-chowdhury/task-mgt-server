1bcb053880568c3b3841b3d57fd0eb79aebcbe44
Nadim Chowdhury <40499378+nadim-chowdhury@users.noreply.github.com>
2025-05-31 12:34:43 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000" || "http://127.0.0.1:3000",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Import routes
